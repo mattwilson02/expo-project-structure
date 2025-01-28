1f9e8758c46b071a73f56297917a938b6bfdef78
mattwilson02 <mattwilsonbmx@gmail.com>
2025-01-28 14:05:19 +0000

    chore: sort out folder structure

---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@sf-digital-ui/react-native";
+import { View } from "react-native";
+
+export default function Signup() {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  );
+}
