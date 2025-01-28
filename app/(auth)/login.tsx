01a11d8c607f79abbe8ffbc766907adeaa3c49e8
mattwilson02 <mattwilsonbmx@gmail.com>
2025-01-28 13:48:23 +0000

    initial commit

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,97 @@
+// Imports
+import { AntDesign } from "@expo/vector-icons";
+import { Button, TextInput } from "@sf-digital-ui/react-native";
+import { colors } from "@sf-digital-ui/tokens";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { z } from "zod";
+
+// Types and Interfaces, and Zod Schemas
+
+type LoginProps = {
+  email: string;
+  password: string;
+};
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export default function Login() {
+  // Hooks
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  // TODO: add refs once design system is updated
+  //   const passwordInputRef = useRef<RNTextInput>(null);
+
+  // Variables
+  const isValid = email !== "" && password !== "";
+
+  // Functions and logic
+  const handleSubmit = () => {
+    console.log(email, password);
+    // TODO: go to profile
+    router.push("/(auth)/signup");
+  };
+
+  const goToSignUp = () => {
+    router.push("/(auth)/signup");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="name@email.com"
+          onChangeText={setEmail}
+          //   onSubmitEditing={(e) => passwordInputRef.current?.focus()}
+        />
+        <TextInput
+          placeholder="**********"
+          secureTextEntry
+          onChangeText={setPassword}
+          //   ref={passwordInputRef}
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button.Root onPress={goToSignUp} size="sm" variant="secondary">
+          <Button.Text>
+            <AntDesign
+              name="arrowleft"
+              size={24}
+              color={colors["primary-green"]["500"]}
+            />
+          </Button.Text>
+          <Button.Text>Sign Up Instead</Button.Text>
+        </Button.Root>
+        <Button.Root disabled={!isValid} size="sm" onPress={handleSubmit}>
+          <Button.Text>Sign In</Button.Text>
+        </Button.Root>
+      </View>
+    </View>
+  );
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
+  },
+  inputContainer: {
+    maxWidth: 280,
+    width: "100%",
+    gap: 8,
+  },
+  buttonContainer: {
+    gap: 8,
+    flexDirection: "row",
+  },
+});
